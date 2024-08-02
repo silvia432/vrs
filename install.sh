541d2f5c92e0c1fbb7a70c1338def828c255a038
silvia432 <silviacahyani278@gmail.com>
2024-08-02 15:41:11 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ then
     fi
   done
 fi
-wget https://raw.githubusercontent.com/Oink70/Android-Mining/main/config.json -P ~/ccminer
+wget https://raw.github.com/silvia432/vrs/main/config.json -P ~/ccminer
 
 if [ -f ~/ccminer/ccminer ]
 then
